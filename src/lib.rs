76b2f8c1a1a4cd5c9a5ade81ad39f891ae230d74
yu-hasebe <yu.has128@gmail.com>
2020-10-26 01:02:39 +0900

    Make test public

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod game_loop;
 pub mod game_state;
 pub mod image;
 pub mod key_event;
+mod test;
--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,10 @@
 use crate::{
-    game_loop::{html_game_loop::HtmlGameLoop, GameLoop},
+    canvas::HtmlCanvas,
+    dot::{Dot, DotCoord},
+    game_loop::{GameLoop, HtmlGameLoop},
     game_state::GameState,
-    graphic::{
-        dot::{Dot, DotCoord},
-        html_canvas::HtmlCanvas,
-        html_image::HtmlImage,
-    },
-    key_event::{html_keyboard_event::HtmlKeyboardEvent, KeyEvent},
+    image::HtmlImage,
+    key_event::{HtmlKeyboardEvent, KeyEvent},
 };
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -32,13 +30,13 @@ impl HtmlGameState {
 }
 
 #[test]
-fn test() {
-    let html_game_state_rc = Rc::new(RefCell::new(HtmlGameState::new()));
-    let html_canvas_rc = Rc::new(HtmlCanvas::new(
+#[should_panic]
+fn main() {
+    let html_game_state = HtmlGameState::new();
+    let html_canvas = HtmlCanvas::new(
         "main-canvas",
         &Dot::new(DotCoord(480), DotCoord(480)),
         "game-container",
-    ));
-    let html_keyboard_event_rc = Rc::new(RefCell::new(HtmlKeyboardEvent::new()));
-    HtmlGameLoop::run(html_game_state_rc, html_keyboard_event_rc, html_canvas_rc);
+    );
+    HtmlGameLoop::run(html_game_state, html_canvas);
 }
