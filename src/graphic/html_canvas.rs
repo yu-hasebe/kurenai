406614b3341bb9c8fec559befcbbe7ca35dcbf6d
yu-hasebe <yu.has128@gmail.com>
2020-10-25 19:37:09 +0900

    Add new function to canvas

--- src/graphic/html_canvas.rs
+++ src/graphic/html_canvas.rs
@@ -1,10 +1,18 @@
 use crate::graphic::{dot::Dot, html_image::HtmlImage};
+use wasm_bindgen::JsCast;
 
 pub struct HtmlCanvas {
     context: web_sys::CanvasRenderingContext2d,
 }
 
 impl HtmlCanvas {
+    pub fn new(id: &str, size: &Dot) -> Self {
+        let canvas = Self::create_html_canvas_element(id, size);
+        // TODO: Add the canvas to an html element
+        let context = Self::context(&canvas);
+        Self { context }
+    }
+
     pub fn draw(
         &self,
         image: &HtmlImage,
@@ -26,5 +34,34 @@ impl HtmlCanvas {
                 size_on_canvas.y().0 as f64,
             )
             .unwrap();
+    }
+
+    fn create_html_canvas_element(id: &str, size: &Dot) -> web_sys::HtmlCanvasElement {
+        let canvas = web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .create_element("canvas")
+            .unwrap();
+        canvas.set_attribute("id", id).unwrap();
+        canvas
+            .set_attribute("width", size.x().0.to_string().as_str())
+            .unwrap();
+        canvas
+            .set_attribute("height", size.y().0.to_string().as_str())
+            .unwrap();
+        canvas
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .map_err(|_| ())
+            .unwrap()
+    }
+
+    fn context(canvas: &web_sys::HtmlCanvasElement) -> web_sys::CanvasRenderingContext2d {
+        canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap()
     }
 }
